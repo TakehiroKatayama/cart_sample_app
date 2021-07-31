7b8fc679fe0f35a93d58d15a812c18256c4b2971
TakehiroKatayama <t.katayama.info@gmail.com>
2021-07-31 13:56:04 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  resources :products
+
+  resources :carts, only: [:show]
+
+  post '/add_item' => 'carts#add_item'
+  post '/update_item' => 'carts#update_item'
+  delete '/delete_item' => 'carts#delete_item'
 end
