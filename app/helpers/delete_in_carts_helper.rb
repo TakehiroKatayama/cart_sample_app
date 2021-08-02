1bdff59a790e0ca73a795117f9a807a7e8e8d82e
TakehiroKatayama <t.katayama.info@gmail.com>
2021-08-02 23:58:51 +0900

    delete_in_cartコントローラーを作成

---
+++ app/helpers/delete_in_carts_helper.rb
@@ -0,0 +1,2 @@
+module DeleteInCartsHelper
+end
---
+++ test/controllers/delete_in_carts_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DeleteInCartsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
