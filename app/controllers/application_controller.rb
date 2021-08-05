bd7f344f1fb38f12cbd05ea065d4bbe993cec27a
TakehiroKatayama <t.katayama.info@gmail.com>
2021-08-05 22:16:14 +0900

    current_cartの記述を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_cart
 
   def current_cart
-    if session[:cart_id]
-      @cart = Cart.find(session[:cart_id])
-    else
-      @cart = Cart.create
-      session[:cart_id] = @cart.id
-    end
-    @cart = Cart.find(session[:cart_id])
-  rescue ActiveRecord::RecordNotFound
-    cart = Cart.create
-    session[:cart_id] = cart.id
-    cart
+    current_cart = Cart.find_by(id: session[:cart_id]) || Cart.create
+    session[:cart_id] ||= current_cart.id
   end
 end
